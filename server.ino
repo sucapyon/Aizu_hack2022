8aa01158d7cee5c5087de8ebea884d427520d8d6
Kengokakazu <kakazukengo62@gmail.com>
2022-08-10 16:16:03 +0900

    finish

--- server.ino
+++ server.ino
@@ -1,11 +1,11 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Servo.h>
 
-
-const char* ssid     = "rx600m-bf5de4-1";
-const char* password = "2fc8ecf29924b";
+const char* ssid     = "SUZAKU_PC";
+const char* password = "12122001";
 const int LED=5;
-
+Servo myservo;
 WebServer server(80);
 String html;
 bool locked = false;
@@ -14,27 +14,28 @@ String  key = "NEKOhakeny";
 void mainPage() 
 {
   Serial.println("mainPage on");
-  html = "<!DOCTYPE html>\n<html lang \"ja\">\n<head>\n<meta charset = \"utf-8\"><meta charset=\"utf-8\">\n  <title>オートロック管理システム</title>\n</head>\n\n<body>\n  <form action=\"http://192.168.1.4/confirm\" method=\"post\" enctype = \"text/plain\">\n    パスワード：<br>\n    <input type=\"text\" name=\"password\">\n    <input type=\"submit\" value = \"確認する\">\n  </form>\n</body>\n</html>";
+  html = "<!DOCTYPE html>\n<html lang \"ja\">\n<head>\n<meta charset = \"utf-8\"><meta charset=\"utf-8\">\n  <title>オートロック管理システム</title>\n</head>\n\n<body>\n  <form action=\"/confirm\" method=\"post\" enctype = \"text/plain\">\n    パスワード：<br>\n    <input type=\"text\" name=\"password\">\n    <input type=\"submit\" value = \"確認する\">\n  </form>\n</body>\n</html>";
   server.send(200,"text/html",html);
+  
   
 }
 void mainPage_2()
 {
-  Serial.println("main_page2 on");
   String form = server.arg("plain");
   Serial.println(form);
   form.replace("password=","");
-  Serial.println(form);
-  if(!form.equals(key))
+  String changed = form;
+  Serial.println(changed);
+  if(changed.indexOf("NEKOhakeny")==-1)
   {
     server.send(200,"text/plain","ThIs password is wrong.");
-    mainPage();
     return;
   }
-  
   html = "\n<html lang \"ja\">\n<head>\n<meta charset=\"utf-8\">\n  <title>ロック管理</title>\n</head>\n<body>\n  <p>\n";
   html += sendState();
-  html += "</p>\n  <form action = \"http://192.168.1.4/lock\" method = \"post\">\n    <input type=\"submit\" value=\"鍵をかける\" id=\"Lock\" oncick=lock(this.id);><br>\n  </form>\n  <form action = \"http://192.168.1.4/unlock\" method = \"post\">\n    <input type=\"submit\" value=\"鍵を開ける\" id=\"unLock\" oncick=unlock(this.id);>\n  </form>\n    <script>\n\n    </script>\n</body>\n</html>\n";
+  html += "</p>\n  <form action = \"/lock\" method = \"post\" enctype = 'text/plain'>\n";
+  html += "<input type=\"hidden\" name='password' value=\"NEKOhakeny\">";
+  html += "<input type=\"submit\" value=\"鍵をかける\" id=\"Lock\" oncick=lock(this.id);><br>\n  </form>\n    <form action = \"/unlock\" method = \"post\" enctype = 'text/plain'>\n <input type = \"hidden\" value = \"NEKOhakeny\" name = \"password\"><input type=\"submit\" value=\"鍵を開ける\" id=\"unLock\" oncick=unlock(this.id);>\n  </form>\n    <script>\n\n    </script>\n</body>\n</html>\n";
   server.send(200,"text/html",html);
 }
 String sendState()
@@ -52,22 +53,22 @@ String sendState()
 void lock()
 {
   Serial.println("lockpage accessed");
-  if(locked)return ;
-  //
+  myservo.write(90);
   locked = true;
-  mainPage_2();  
+   mainPage_2();
+  
 }
 void unlock()
 {
   Serial.println("unLockpage accessed");
-  if(!locked)return ;
-  //
+  myservo.write(0);
   locked = false;
   mainPage_2();
 }
 void setup(void) 
 {
   pinMode(LED,OUTPUT);
+  myservo.attach(2); 
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
