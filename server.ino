a905d477de9b1eb5bd75a33cf62dba400cd594cd
Kengokakazu <kakazukengo62@gmail.com>
2022-08-11 20:07:26 +0900

    パスワード変更機能追加

--- server.ino
+++ server.ino
@@ -12,7 +12,6 @@ long interval = 1000;
 String html;
 bool locked = false;
 String  key = "NEKOhakeny";
-
 void mainPage() 
 {
   Serial.println("mainPage on");
@@ -28,7 +27,7 @@ void mainPage_2()
   form.replace("password=","");
   String changed = form;
   Serial.println(changed);
-  if(changed.indexOf("NEKOhakeny")==-1)
+  if(changed.indexOf(key)==-1)
   {
     server.send(200,"text/plain","ThIs password is wrong.");
     return;
@@ -36,8 +35,9 @@ void mainPage_2()
   html = "\n<html lang \"ja\">\n<head>\n<meta charset=\"utf-8\">\n  <title>ロック管理</title>\n</head>\n<body>\n  <p>\n";
   html += sendState();
   html += "</p>\n  <form action = \"/lock\" method = \"post\" enctype = 'text/plain'>\n";
-  html += "<input type=\"hidden\" name='password' value=\"NEKOhakeny\">";
-  html += "<input type=\"submit\" value=\"鍵をかける\" id=\"Lock\" oncick=lock(this.id);><br>\n  </form>\n    <form action = \"/unlock\" method = \"post\" enctype = 'text/plain'>\n <input type = \"hidden\" value = \"NEKOhakeny\" name = \"password\"><input type=\"submit\" value=\"鍵を開ける\" id=\"unLock\" oncick=unlock(this.id);>\n  </form>\n    <script>\n\n    </script>\n</body>\n</html>\n";
+  html += "<input type=\"hidden\" name='password' value=\"" + key + "\">";
+  html += "<input type=\"submit\" value=\"鍵をかける\" id=\"Lock\" oncick=lock(this.id);><br>\n  </form>\n    <form action = \"/unlock\" method = \"post\" enctype = 'text/plain'>\n <input type = \"hidden\" value=\"" + key + "\" name = \"password\"><input type=\"submit\" value=\"鍵を開ける\" id=\"unLock\" oncick=unlock(this.id);>\n  </form>\n    <script>\n\n    </script>\n</body>\n</html>\n";
+  html += "<br>\n      <form action = '/change' method = 'post' enctype = 'text/plain'>\n <input type='submit' value='パスワード変更' id='unLock' oncick=unlock(this.id);>\n  </form>";
   server.send(200,"text/html",html);
 }
 String sendState()
@@ -50,6 +50,17 @@ String sendState()
   {
     return "鍵はロックされていません";
   }
+}
+void change_password()
+{
+  html ="<!DOCTYPE html>\n<html lang 'ja'>\n\n<head>\n  <meta charset='utf-8'>\n <title>オートロック管理システム</title>\n</head>\n\n<body>\n";
+  html += "<form action='/confirm' method='post' enctype='text/plain'>\n    新しいパスワード：<br>\n    <input type='text' name='password'>\n    <input type='submit' value='変更する'>\n  </form>";
+  html += "<input type = 'hidden' value= '" + key + "' name = 'password'><br>\n";
+  html += "\n</body>\n\n</html>'";
+  server.send(200,"text/html",html);
+  String new_key = server.arg("plain");
+  mainPage_2();
+  key = new_key;
 }
 
 void lock()
@@ -76,7 +87,7 @@ void auto_lock()
   unsigned long currentMillis = millis();
   if(currentMillis - previousMillis > interval) 
   {
-    Serial.println("５ふんたちました");
+    //Serial.println("５ふんたちました");
     if(!locked)myservo.write(0);
     locked = true;
     previousMillis = currentMillis;
@@ -105,6 +116,7 @@ void setup(void)
   server.on("/confirm",mainPage_2);
   server.on("/lock",lock);
   server.on("/unlock",unlock);
+  server.on("/change",change_password);
   server.begin();
 }
 void loop(void) 
